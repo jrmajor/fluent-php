fa079e2348b939fa808ebc91b6ca11f9887483ec
Jeremiasz Major <jrh.mjr@gmail.com>
2021-05-26 16:11:20 +0200

    formatting

--- tests/Parser/Literals/NumberLiteralTest.php
+++ tests/Parser/Literals/NumberLiteralTest.php
@@ -2,100 +2,34 @@
 
 use function App\Tests\parseLiteral;
 
-it('can parse integers', function () {
-    expect(parseLiteral('x = {0}'))
-        ->toHaveProperty('value', 0.0)
-        ->toHaveProperty('precision', 0);
+it('can parse integers')
+    ->expect(parseLiteral('x = {0}'))->toHaveProperty('value', 0.0)->toHaveProperty('precision', 0)
+    ->and(parseLiteral('x = {1}'))->toHaveProperty('value', 1.0)->toHaveProperty('precision', 0)
+    ->and(parseLiteral('x = {-1}'))->toHaveProperty('value', -1.0)->toHaveProperty('precision', 0)
+    ->and(parseLiteral('x = {-0}'))->toHaveProperty('value', 0.0)->toHaveProperty('precision', 0);
 
-    expect(parseLiteral('x = {1}'))
-        ->toHaveProperty('value', 1.0)
-        ->toHaveProperty('precision', 0);
+it('can parse padded integers')
+    ->expect(parseLiteral('x = {01}'))->toHaveProperty('value', 1.0)->toHaveProperty('precision', 0)
+    ->and(parseLiteral('x = {-01}'))->toHaveProperty('value', -1.0)->toHaveProperty('precision', 0)
+    ->and(parseLiteral('x = {00}'))->toHaveProperty('value', 0.0)->toHaveProperty('precision', 0)
+    ->and(parseLiteral('x = {-00}'))->toHaveProperty('value', 0.0)->toHaveProperty('precision', 0);
 
-    expect(parseLiteral('x = {-1}'))
-        ->toHaveProperty('value', -1.0)
-        ->toHaveProperty('precision', 0);
+it('can parse positive floats')
+    ->expect(parseLiteral('x = {0.0}'))->toHaveProperty('value', 0.0)->toHaveProperty('precision', 1)
+    ->and(parseLiteral('x = {0.01}'))->toHaveProperty('value', 0.01)->toHaveProperty('precision', 2)
+    ->and(parseLiteral('x = {1.03}'))->toHaveProperty('value', 1.03)->toHaveProperty('precision', 2)
+    ->and(parseLiteral('x = {1.000}'))->toHaveProperty('value', 1.0)->toHaveProperty('precision', 3);
 
-    expect(parseLiteral('x = {-0}'))
-        ->toHaveProperty('value', 0.0)
-        ->toHaveProperty('precision', 0);
-});
+it('can parse negative floats')
+    ->expect(parseLiteral('x = {-0.01}'))->toHaveProperty('value', -0.01)->toHaveProperty('precision', 2)
+    ->and(parseLiteral('x = {-1.03}'))->toHaveProperty('value', -1.03)->toHaveProperty('precision', 2)
+    ->and(parseLiteral('x = {-0.0}'))->toHaveProperty('value', 0.0)->toHaveProperty('precision', 1)
+    ->and(parseLiteral('x = {-1.000}'))->toHaveProperty('value', -1.0)->toHaveProperty('precision', 3);
 
-it('can parse padded integers', function () {
-    expect(parseLiteral('x = {01}'))
-        ->toHaveProperty('value', 1.0)
-        ->toHaveProperty('precision', 0);
-
-    expect(parseLiteral('x = {-01}'))
-        ->toHaveProperty('value', -1.0)
-        ->toHaveProperty('precision', 0);
-
-    expect(parseLiteral('x = {00}'))
-        ->toHaveProperty('value', 0.0)
-        ->toHaveProperty('precision', 0);
-
-    expect(parseLiteral('x = {-00}'))
-        ->toHaveProperty('value', 0.0)
-        ->toHaveProperty('precision', 0);
-});
-
-it('can parse positive floats', function () {
-    expect(parseLiteral('x = {0.0}'))
-        ->toHaveProperty('value', 0.0)
-        ->toHaveProperty('precision', 1);
-
-    expect(parseLiteral('x = {0.01}'))
-        ->toHaveProperty('value', 0.01)
-        ->toHaveProperty('precision', 2);
-
-    expect(parseLiteral('x = {1.03}'))
-        ->toHaveProperty('value', 1.03)
-        ->toHaveProperty('precision', 2);
-
-    expect(parseLiteral('x = {1.000}'))
-        ->toHaveProperty('value', 1.0)
-        ->toHaveProperty('precision', 3);
-});
-
-it('can parse negative floats', function () {
-    expect(parseLiteral('x = {-0.01}'))
-        ->toHaveProperty('value', -0.01)
-        ->toHaveProperty('precision', 2);
-
-    expect(parseLiteral('x = {-1.03}'))
-        ->toHaveProperty('value', -1.03)
-        ->toHaveProperty('precision', 2);
-
-    expect(parseLiteral('x = {-0.0}'))
-        ->toHaveProperty('value', 0.0)
-        ->toHaveProperty('precision', 1);
-
-    expect(parseLiteral('x = {-1.000}'))
-        ->toHaveProperty('value', -1.0)
-        ->toHaveProperty('precision', 3);
-});
-
-it('can parse padded floats', function () {
-    expect(parseLiteral('x = {01.03}'))
-        ->toHaveProperty('value', 1.03)
-        ->toHaveProperty('precision', 2);
-
-    expect(parseLiteral('x = {1.0300}'))
-        ->toHaveProperty('value', 1.03)
-        ->toHaveProperty('precision', 4);
-
-    expect(parseLiteral('x = {01.0300}'))
-        ->toHaveProperty('value', 1.03)
-        ->toHaveProperty('precision', 4);
-
-    expect(parseLiteral('x = {-01.03}'))
-        ->toHaveProperty('value', -1.03)
-        ->toHaveProperty('precision', 2);
-
-    expect(parseLiteral('x = {-1.0300}'))
-        ->toHaveProperty('value', -1.03)
-        ->toHaveProperty('precision', 4);
-
-    expect(parseLiteral('x = {-01.0300}'))
-        ->toHaveProperty('value', -1.03)
-        ->toHaveProperty('precision', 4);
-});
+it('can parse padded floats')
+    ->expect(parseLiteral('x = {01.03}'))->toHaveProperty('value', 1.03)->toHaveProperty('precision', 2)
+    ->and(parseLiteral('x = {1.0300}'))->toHaveProperty('value', 1.03)->toHaveProperty('precision', 4)
+    ->and(parseLiteral('x = {01.0300}'))->toHaveProperty('value', 1.03)->toHaveProperty('precision', 4)
+    ->and(parseLiteral('x = {-01.03}'))->toHaveProperty('value', -1.03)->toHaveProperty('precision', 2)
+    ->and(parseLiteral('x = {-1.0300}'))->toHaveProperty('value', -1.03)->toHaveProperty('precision', 4)
+    ->and(parseLiteral('x = {-01.0300}'))->toHaveProperty('value', -1.03)->toHaveProperty('precision', 4);
--- tests/Parser/Literals/StringLiteralTest.php
+++ tests/Parser/Literals/StringLiteralTest.php
@@ -2,48 +2,24 @@
 
 use function App\Tests\parseLiteral;
 
-it('can parse string literal with no escape sequences', function () {
-    expect(parseLiteral('x = {"abc"}'))
-        ->toHaveProperty('value', 'abc');
-});
+it('can parse string literal with no escape sequences')
+    ->expect(parseLiteral('x = {"abc"}'))->toHaveProperty('value', 'abc');
 
-it('properly escapes backslash and double quote', function () {
-    expect(parseLiteral('x = {"\\\\"}'))
-        ->toHaveProperty('value', '\\');
+it('properly escapes backslash and double quote')
+    ->expect(parseLiteral('x = {"\\\\"}'))->toHaveProperty('value', '\\')
+    ->and(parseLiteral('x = {"\\""}'))->toHaveProperty('value', '"')
+    ->and(parseLiteral('x = {"\\\\\\""}'))->toHaveProperty('value', '\\"');
 
-    expect(parseLiteral('x = {"\\""}'))
-        ->toHaveProperty('value', '"');
+it('can parse unicode escapes')
+    ->expect(parseLiteral('x = {"\\u0041"}'))->toHaveProperty('value', 'A')
+    ->and(parseLiteral('x = {"\\\\u0041"}'))->toHaveProperty('value', '\\u0041')
+    ->and(parseLiteral('x = {"\\U01F602"}'))->toHaveProperty('value', '😂')
+    ->and(parseLiteral('x = {"\\\\U01F602"}'))->toHaveProperty('value', '\\U01F602');
 
-    expect(parseLiteral('x = {"\\\\\\""}'))
-        ->toHaveProperty('value', '\\"');
-});
+test('trailing zeroes are not part unicode escapes')
+    ->expect(parseLiteral('x = {"\\u004100"}'))->toHaveProperty('value', 'A00')
+    ->and(parseLiteral('x = {"\\U01F60200"}'))->toHaveProperty('value', '😂00');
 
-it('can parse unicode escapes', function () {
-    expect(parseLiteral('x = {"\\u0041"}'))
-        ->toHaveProperty('value', 'A');
-
-    expect(parseLiteral('x = {"\\\\u0041"}'))
-        ->toHaveProperty('value', '\\u0041');
-
-    expect(parseLiteral('x = {"\\U01F602"}'))
-        ->toHaveProperty('value', '😂');
-
-    expect(parseLiteral('x = {"\\\\U01F602"}'))
-        ->toHaveProperty('value', '\\U01F602');
-});
-
-test('trailing zeroes are not part unicode escapes', function () {
-    expect(parseLiteral('x = {"\\u004100"}'))
-        ->toHaveProperty('value', 'A00');
-
-    expect(parseLiteral('x = {"\\U01F60200"}'))
-        ->toHaveProperty('value', '😂00');
-});
-
-it('properly parses literal braces', function () {
-    expect(parseLiteral('x = {"{"}'))
-        ->toHaveProperty('value', '{');
-
-    expect(parseLiteral('x = {"}"}'))
-        ->toHaveProperty('value', '}');
-});
+it('properly parses literal braces')
+    ->expect(parseLiteral('x = {"{"}'))->toHaveProperty('value', '{')
+    ->and(parseLiteral('x = {"}"}'))->toHaveProperty('value', '}');
