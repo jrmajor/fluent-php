efdaa70e803dfcce5e173d24309e444c5f5904b2
Jeremiasz Major <jrh.mjr@gmail.com>
2021-11-05 03:35:31 +0100

    make exception constructors internal

--- src/Exceptions/Resolver/CyclicReferenceException.php
+++ src/Exceptions/Resolver/CyclicReferenceException.php
@@ -4,6 +4,9 @@ namespace Major\Fluent\Exceptions\Resolver;
 
 final class CyclicReferenceException extends RangeException
 {
+    /**
+     * @internal
+     */
     public function __construct()
     {
         parent::__construct('Placeable contains cyclic reference.');
