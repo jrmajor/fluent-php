ccaffd4475790b8cb45a760f6da0770f054a4f99
Jeremiasz Major <jrh.mjr@gmail.com>
2021-11-08 20:09:08 +0100

    conver node tests to phpunit

--- tests/Node/BaseNodeTest.php
+++ tests/Node/BaseNodeTest.php
@@ -1,12 +1,22 @@
 <?php
 
+namespace Major\Fluent\Tests\Node;
+
 use Major\Fluent\Node\Syntax\Expressions\Literals\StringLiteral;
 use Major\Fluent\Node\Syntax\Identifier;
+use Major\Fluent\Tests\TestCase;
 
-test('getType() method works', function () {
-    $identifier = new Identifier('name');
-    $stringLiteral = new StringLiteral('name');
+final class BaseNodeTest extends TestCase
+{
+    /**
+     * @testdox getType() method works
+     */
+    public function testGetType(): void
+    {
+        $identifier = new Identifier('name');
+        $stringLiteral = new StringLiteral('name');
 
-    expect($identifier->getType())->toBe('Identifier');
-    expect($stringLiteral->getType())->toBe('StringLiteral');
-});
+        $this->assertSame('Identifier', $identifier->getType());
+        $this->assertSame('StringLiteral', $stringLiteral->getType());
+    }
+}
--- tests/Node/NodeEqualsAssertionTest.php
+++ tests/Node/NodeEqualsAssertionTest.php
@@ -1,54 +1,64 @@
 <?php
 
+namespace Major\Fluent\Tests\Node;
+
 use Major\Fluent\Node\Syntax\Attribute;
 use Major\Fluent\Node\Syntax\Entries\Message;
 use Major\Fluent\Node\Syntax\Identifier;
 use Major\Fluent\Node\Syntax\Patterns\Pattern;
+use Major\Fluent\Tests\TestCase;
 
-test('assertNodeEquals assertion works', function () {
-    $identifier = new Message(
-        id: new Identifier('tst'),
-        value: null,
-        attributes: [
-            new Attribute(
-                id: new Identifier('asef'),
-                value: new Pattern([]),
-            ),
-        ],
-        comment: null,
-    );
+final class NodeEqualsAssertionTest extends TestCase
+{
+    /**
+     * @testdox assertNodeEquals assertion works
+     */
+    public function testAssertion(): void
+    {
+        $identifier = new Message(
+            id: new Identifier('tst'),
+            value: null,
+            attributes: [
+                new Attribute(
+                    id: new Identifier('asef'),
+                    value: new Pattern([]),
+                ),
+            ],
+            comment: null,
+        );
 
-    $expected = [
-        'type' => 'Message',
-        'id' => [
-            'type' => 'Identifier',
-            'name' => 'tst',
-            'span' => null,
-        ],
-        'span' => null,
-        'value' => null,
-        'comment' => null,
-        'attributes' => [
-            [
-                'type' => 'Attribute',
-                'id' => [
-                    'type' => 'Identifier',
-                    'name' => 'asef',
-                    'span' => null,
-                ],
-                'value' => [
-                    'type' => 'Pattern',
-                    'elements' => [],
-                    'span' => null,
-                ],
+        $expected = [
+            'type' => 'Message',
+            'id' => [
+                'type' => 'Identifier',
+                'name' => 'tst',
                 'span' => null,
             ],
-        ],
-    ];
+            'span' => null,
+            'value' => null,
+            'comment' => null,
+            'attributes' => [
+                [
+                    'type' => 'Attribute',
+                    'id' => [
+                        'type' => 'Identifier',
+                        'name' => 'asef',
+                        'span' => null,
+                    ],
+                    'value' => [
+                        'type' => 'Pattern',
+                        'elements' => [],
+                        'span' => null,
+                    ],
+                    'span' => null,
+                ],
+            ],
+        ];
 
-    $this->assertNodeEquals(
-        $expected, $identifier,
-        spans: true,
-        annotations: true,
-    );
-});
+        $this->assertNodeEquals(
+            $expected, $identifier,
+            spans: true,
+            annotations: true,
+        );
+    }
+}
