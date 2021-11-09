f1241dccbe33cc5a9c9c6a6840a7fbe8ebd4d4b5
Jeremiasz Major <jrh.mjr@gmail.com>
2021-11-09 10:56:33 +0100

    convert resolver/selects tests to phpunit

--- tests/Bundle/Resolver/SelectExpressions/PluralCategoriesTest.php
+++ tests/Bundle/Resolver/SelectExpressions/PluralCategoriesTest.php
@@ -3,70 +3,106 @@
 namespace Major\Fluent\Tests\Bundle\Resolver\SelectExpressions;
 
 use Major\Fluent\Bundle\FluentBundle;
+use Major\Fluent\Tests\TestCase;
 
-$bundle = (new FluentBundle('en-US', strict: true, useIsolating: false))
-    ->addFtl(<<<'ftl'
-        matching = { $selector ->
-            [one] A
-           *[other] B
-        }
+final class PluralCategoriesTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
 
-        non-matching = { $selector ->
-            [one] A
-           *[default] D
-        }
-        ftl);
+        $this->bundle->addFtl(<<<'ftl'
+            matching = { $selector ->
+                [one] A
+               *[other] B
+            }
 
-it('returns variant for matching number selector')
-    ->expect($bundle->message('matching', selector: 1))->toBe('A');
+            non-matching = { $selector ->
+                [one] A
+               *[default] D
+            }
+            ftl);
+    }
 
-it('returns variant for matching string selector')
-    ->expect($bundle->message('matching', selector: 'one'))->toBe('A');
+    /**
+     * @testdox it returns variant for matching number selector
+     */
+    public function testNumber(): void
+    {
+        $this->assertTranslation('A', 'matching', ['selector' => 1]);
+    }
 
-it('falls back to default variant for non-matching number selector')
-    ->expect($bundle->message('non-matching', selector: 2))->toBe('D');
+    /**
+     * @testdox it returns variant for matching string selector
+     */
+    public function testString(): void
+    {
+        $this->assertTranslation('A', 'matching', ['selector' => 'one']);
+    }
 
-it('falls back to default variant for non-matching string selector')
-    ->expect($bundle->message('non-matching', selector: 'other'))->toBe('D');
+    /**
+     * @testdox it falls back to default variant for non-matching number selector
+     */
+    public function testNumberFallback(): void
+    {
+        $this->assertTranslation('D', 'non-matching', ['selector' => 2]);
+    }
 
-$bundle = (new FluentBundle('en-EN', strict: true, useIsolating: false))
-    ->addFtl(<<<'ftl'
-        msg = { $arg ->
-            [one] example
-           *[other] examples
-        }
-        ftl);
+    /**
+     * @testdox it falls back to default variant for non-matching string selector
+     */
+    public function testStringFallback(): void
+    {
+        $this->assertTranslation('D', 'non-matching', ['selector' => 'other']);
+    }
 
-test('plural categories in english')
-    ->expect($bundle->message('msg', arg: 1))->toBe('example')
-    ->and($bundle->message('msg', arg: 2))->toBe('examples')
-    ->and($bundle->message('msg', arg: 5))->toBe('examples')
-    ->and($bundle->message('msg', arg: 1.1))->toBe('examples')
-    ->and($bundle->message('msg', arg: 2.5))->toBe('examples');
+    /**
+     * @testdox plural categories in english
+     */
+    public function testPluralEnglish(): void
+    {
+        $bundle = (new FluentBundle('en-EN', useIsolating: false))->addFtl(<<<'ftl'
+            msg = { $arg ->
+                [one] example
+               *[other] examples
+            }
+            ftl);
 
-$bundle = (new FluentBundle('pl-PL', strict: true, useIsolating: false))
-    ->addFtl(<<<'ftl'
-        msg = { $arg ->
-            [one] przykład
-            [few] przykłady
-            [many] przykładów
-           *[other] przykładu
-        }
-        ftl);
+        $this->assertTranslation('example', 'msg', ['arg' => 1], $bundle);
+        $this->assertTranslation('examples', 'msg', ['arg' => 2], $bundle);
+        $this->assertTranslation('examples', 'msg', ['arg' => 5], $bundle);
+        $this->assertTranslation('examples', 'msg', ['arg' => 1.1], $bundle);
+        $this->assertTranslation('examples', 'msg', ['arg' => 2.5], $bundle);
+    }
 
-test('plural categories in polish')
-    ->expect($bundle->message('msg', arg: 1))->toBe('przykład')
-    ->and($bundle->message('msg', arg: 2))->toBe('przykłady')
-    ->and($bundle->message('msg', arg: 3))->toBe('przykłady')
-    ->and($bundle->message('msg', arg: 4))->toBe('przykłady')
-    ->and($bundle->message('msg', arg: 5))->toBe('przykładów')
-    ->and($bundle->message('msg', arg: 6))->toBe('przykładów')
-    ->and($bundle->message('msg', arg: 11))->toBe('przykładów')
-    ->and($bundle->message('msg', arg: 12))->toBe('przykładów')
-    ->and($bundle->message('msg', arg: 14))->toBe('przykładów')
-    ->and($bundle->message('msg', arg: 21))->toBe('przykładów')
-    ->and($bundle->message('msg', arg: 22))->toBe('przykłady')
-    ->and($bundle->message('msg', arg: 23))->toBe('przykłady')
-    ->and($bundle->message('msg', arg: 25))->toBe('przykładów')
-    ->and($bundle->message('msg', arg: 1.1))->toBe('przykładu')
-    ->and($bundle->message('msg', arg: 2.5))->toBe('przykładu');
+    /**
+     * @testdox plural categories in polish
+     */
+    public function testPluralPolish(): void
+    {
+        $bundle = (new FluentBundle('pl-PL', useIsolating: false))->addFtl(<<<'ftl'
+            msg = { $arg ->
+                [one] przykład
+                [few] przykłady
+                [many] przykładów
+               *[other] przykładu
+            }
+            ftl);
+
+        $this->assertTranslation('przykład', 'msg', ['arg' => 1], $bundle);
+        $this->assertTranslation('przykłady', 'msg', ['arg' => 2], $bundle);
+        $this->assertTranslation('przykłady', 'msg', ['arg' => 3], $bundle);
+        $this->assertTranslation('przykłady', 'msg', ['arg' => 4], $bundle);
+        $this->assertTranslation('przykładów', 'msg', ['arg' => 5], $bundle);
+        $this->assertTranslation('przykładów', 'msg', ['arg' => 6], $bundle);
+        $this->assertTranslation('przykładów', 'msg', ['arg' => 11], $bundle);
+        $this->assertTranslation('przykładów', 'msg', ['arg' => 12], $bundle);
+        $this->assertTranslation('przykładów', 'msg', ['arg' => 14], $bundle);
+        $this->assertTranslation('przykładów', 'msg', ['arg' => 21], $bundle);
+        $this->assertTranslation('przykłady', 'msg', ['arg' => 22], $bundle);
+        $this->assertTranslation('przykłady', 'msg', ['arg' => 23], $bundle);
+        $this->assertTranslation('przykładów', 'msg', ['arg' => 25], $bundle);
+        $this->assertTranslation('przykładu', 'msg', ['arg' => 1.1], $bundle);
+        $this->assertTranslation('przykładu', 'msg', ['arg' => 2.5], $bundle);
+    }
+}
