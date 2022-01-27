83e088b2fc74c0748e7cd2eece05f1f72a5a53ed
Jeremiasz Major <jrh.mjr@gmail.com>
2022-01-27 05:04:13 +0100

    add more cursor benchmarks

--- tests/Benchmark/CursorBench.php
+++ tests/Benchmark/CursorBench.php
@@ -13,19 +13,57 @@ final class CursorBench
 {
     private FluentCursor $cursor;
 
+    private int $length;
+
     public function setUp(): void
     {
         // File obtained from https://hg.mozilla.org/l10n-central/pl/file/tip/browser/browser/preferences/preferences.ftl.
         $preferences = Filesystem\read_file(__DIR__ . '/preferences.ftl');
 
         $this->cursor = new FluentCursor($preferences);
+        $this->length = mb_strlen($preferences);
     }
 
     #[Revs(20), Iterations(10)]
-    public function benchNextAndCurrentChar(): void
+    public function benchNext(): void
     {
-        while ($this->cursor->currentChar() !== null) {
+        for ($i = 0; $i < $this->length; $i++) {
             $this->cursor->next();
+        }
+    }
+
+    #[Revs(20), Iterations(10)]
+    public function benchCurrentChar(): void
+    {
+        for ($i = 0; $i < $this->length; $i++) {
+            $this->cursor->currentChar();
+            $this->cursor->next();
+        }
+    }
+
+    #[Revs(20), Iterations(10)]
+    public function benchCurrentPeek(): void
+    {
+        for ($i = 0; $i < $this->length; $i++) {
+            $this->cursor->currentPeek();
+            $this->cursor->peek();
+
+            if ($this->cursor->peekOffset() > 100) {
+                $this->cursor->skipToPeek();
+            }
+        }
+    }
+
+    #[Revs(20), Iterations(10)]
+    public function benchTakeChar(): void
+    {
+        for ($i = 0; $i < $this->length; $i++) {
+            $this->cursor->takeChar(function (string $c): bool {
+                $cc = mb_ord($c);
+
+                return ($cc >= 97 && $cc <= 122) // a-z
+                    || ($cc >= 65 && $cc <= 90); // A-Z
+            }) ?? $this->cursor->next();
         }
     }
 }
