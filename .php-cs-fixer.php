ca85b5338990406871576e39ff6a483556d17960
Jeremiasz Major <jrh.mjr@gmail.com>
2022-06-19 19:27:55 +0200

    fix php-cs-fixer configuration

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,7 +2,8 @@
 
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
-    ->ignoreVCS(true);
+    ->append(['gen'])
+    ->ignoreVCSIgnored(true);
 
 return Major\CS\config($finder, [
     // Remove constructor, wanted to have it after public methods when it's private.
@@ -10,8 +11,4 @@ return Major\CS\config($finder, [
         'order' => ['use_trait', 'constant', 'property', 'method', 'magic'],
     ],
     'phpdoc_summary' => false,
-    // Remove throw from tokens, removes blank line when throw is used as an expression.
-    'no_extra_blank_lines' => [
-        'tokens' => ['case', 'continue', 'curly_brace_block', 'default', 'extra', 'parenthesis_brace_block', 'switch'],
-    ],
 ])->setCacheFile('.cache/.php-cs-fixer.cache');
